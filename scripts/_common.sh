0e6728b29606f891c55fcd375ab33fef3a9457b8
discapacidad5 <discapacidad5@gmail.com>
2021-01-16 13:41:44 -0400

    Create _common.sh

---
+++ scripts/_common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="php-cli php-mysql php-json php-gd php-tidy php-curl php-gettext php-redis"
